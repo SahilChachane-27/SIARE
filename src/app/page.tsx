daa03ca1737f4293d5bad44bc65256e890be1f27
SAHIL CHACHANE <sahilchachane11@gmail.com>
2026-03-15 11:51:48 +0000

    remove this section  (_for element img_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,9 @@ import { Header } from '@/components/layout/header';
 import { Footer } from '@/components/layout/footer';
 import { Hero } from '@/components/sections/hero';
 import { Partners } from '@/components/sections/partners';
-import { About } from '@/components/sections/about';
-import { Services } from '@/components/sections/services';
 import { Projects } from '@/components/sections/projects';
 import { ScrollToTop } from '@/components/layout/scroll-to-top';
 import { MembershipHighlights } from '@/components/sections/membership-highlights';
-import { LearningHub } from '@/components/sections/learning-hub';
 import { Testimonials } from '@/components/sections/testimonials';
 
 export default function Home() {
@@ -18,11 +15,8 @@ export default function Home() {
       <Header />
       <main className="flex-1 overflow-x-hidden">
         <Hero />
-        <About />
-        {/* <Services /> */}
         <Projects />
         <MembershipHighlights />
-        <LearningHub />
         <Testimonials />
         <Partners />
       </main>
