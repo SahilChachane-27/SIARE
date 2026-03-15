0de9b166de7ed2462b32953c85883d1ca73e325b
SAHIL CHACHANE <sahilchachane11@gmail.com>
2026-03-15 09:12:52 +0000

    Make the following changes:
    Find this element:
    
      <div class="container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import { Partners } from '@/components/sections/partners';
 import { About } from '@/components/sections/about';
 import { Services } from '@/components/sections/services';
 import { Projects } from '@/components/sections/projects';
-import { WhyUs } from '@/components/sections/why-us';
 import { ScrollToTop } from '@/components/layout/scroll-to-top';
 import { MembershipHighlights } from '@/components/sections/membership-highlights';
 import { LearningHub } from '@/components/sections/learning-hub';
@@ -19,7 +18,6 @@ export default function Home() {
       <Header />
       <main className="flex-1 overflow-x-hidden">
         <Hero />
-        <WhyUs />
         <About />
         <Services />
         <Projects />
